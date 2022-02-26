bc744d07248ccb367dc66d586f676168d2a1dff0
shuaipengcheng1 <1194721054@qq.com>
2022-02-26 15:54:22 +0800

    Day7

---
+++ JVM/Demo1/src/main/java/com.misaka.java/SetOldOrNewAreaPercent.java
@@ -0,0 +1,13 @@
+package com.misaka.java;
+
+public class SetOldOrNewAreaPercent {
+//    设置堆空间中的 老年代和新生代的在堆空间的比例
+public static void main(String[] args) {
+    System.out.println("启动");
+    try {
+        Thread.sleep(100000);
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+}
+}

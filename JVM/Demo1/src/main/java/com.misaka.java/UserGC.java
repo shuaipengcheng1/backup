a48f8ba8ac35d325d44c990eadfb70cdcffe9a64
shuaipengcheng1 <1194721054@qq.com>
2022-03-11 17:31:45 +0800

    Day18

--- JVM/Demo1/src/main/java/com.misaka.java/UserGC.java
+++ JVM/Demo1/src/main/java/com.misaka.java/UserGC.java
@@ -7,7 +7,8 @@ public class UserGC {
 //    设置为serialGC(串行)
 //    -XX:+UseSerialGC 标识新生代使用SerialGC 老年代使用SerialOldGC
 //    -XX:+UseParNewGC(并行) 标识新生代使用 ParNew 老年代使用 Serial Old
-//
+//    -XX:+UseConcMarkSweepGC(并发) 启用CMS
+//    -XX:+UseG1GC(并发 分区算法 堆区综合回收)
 public static void main(String[] args) {
     try {
         Thread.sleep(10000000);

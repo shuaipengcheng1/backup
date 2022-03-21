7c1e4af0a71a1e647a8341b8a5c325a33c677f82
shuaipengcheng1 <1194721054@qq.com>
2022-03-21 19:16:21 +0800

    Day25

---
+++ JVM/Demo1/src/main/java/com/misaka/java/中篇/whileTest.java
@@ -0,0 +1,10 @@
+package com.misaka.java.中篇;
+
+public class whileTest {
+    public static void main(String[] args) {
+        int i=0;
+        while (i<100){
+            i++;
+        }
+    }
+}
